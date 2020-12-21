8da45967491757d6cc4fb82491d90ffe2da4462c
chr233 <chr@chrxw.com>
2020-12-22 01:24:06 +0800

    update

--- swhweb/urls.py
+++ swhweb/urls.py
@@ -1,8 +1,12 @@
-# import debug_toolbar
+
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from app import views
 from rest_framework.authtoken.views import obtain_auth_token
+
+from django.conf import settings
+
+DEBUG = settings.DEBUG
 
 visitor_router = DefaultRouter()
 visitor_router.register(r'games', views.GameSimpleInfoViewSet, 'A')
@@ -21,10 +25,16 @@ admin_router.register(r'access', views.AccessStatsViewSet, 'B')
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api-token-auth/', obtain_auth_token),
     path('', include(visitor_router.urls)),
-    path('adv/', include(admin_router.urls)),
-    # path('test', views.test),
-    # path('__debug__/', include(debug_toolbar.urls))
+    path('adv/', include(admin_router.urls))
 ]
+
+
+if DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+        path('api-token-auth/', obtain_auth_token),
+        path('test', views.test),
+        path('__debug__/', include(debug_toolbar.urls))
+    ]
