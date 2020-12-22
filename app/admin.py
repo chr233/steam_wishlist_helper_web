901cad5720a90046600983a5373ce9211d391d29
chr233 <chr@chrxw.com>
2020-12-23 01:45:41 +0800

    update

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,3 @@
 # -*- coding: UTF-8 -*-
-from rest_framework.authtoken.admin import TokenAdmin
-TokenAdmin.raw_id_fields = ['user']
+# from rest_framework.authtoken.admin import TokenAdmin
+# TokenAdmin.raw_id_fields = ['user']
